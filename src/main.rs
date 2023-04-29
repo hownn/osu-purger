428015dfe2c1d00536d107d3f86e8dd89b2d9711
Jett <jettosu@proton.me>
2023-04-29 19:33:10 +0200

    made function calls a one-liner

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,6 @@ mod index_files;
 mod delete_files;
 
 fn main() {
-    // gets path for osu! songs directory
-    let path = get_path::get_path();
-
-    // files at the given path are indexed into a vector
-    let indexed_files = index_files::index_files(path);
-    
-    // deletes the filtered files
-    delete_files::delete_files(indexed_files);
+    // gets path for files, indexes based on extension and deletes matching files
+    delete_files::delete_files(index_files::index_files(get_path::get_path()));
 }
